0fd8940e8ed4438045760ff7cf6b7e6b259d57ec
tvaldiviana6503 <tvaldiviana6503@student.pps.net>
2023-05-10 13:50:02 -0700

    not done

---
+++ Assets/Text.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Text : MonoBehaviour
+{
+    public GameObject UiObject;
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UiObject.SetActive(false);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            UiObject.SetActive(true);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        UiObject.SetActive(false);
+    }
+}
