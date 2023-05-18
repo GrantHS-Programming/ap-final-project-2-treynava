e49157d73822f2fc4221e3a98553ce783c9aab3a
tvaldiviana6503 <tvaldiviana6503@student.pps.net>
2023-05-18 14:02:12 -0700

    moved bus

---
+++ Assets/MoveDirection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDirection : MonoBehaviour
+{
+    public GameObject objectToMove;
+    public Vector3 moveDirection;
+    public float speed = 3.0f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        objectToMove.transform.position += moveDirection * speed;
+    }
+}
