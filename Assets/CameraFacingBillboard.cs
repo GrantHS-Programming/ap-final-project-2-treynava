aeb580ab119aed914e09359d2c8bc1f0952dcd8e
tvaldiviana6503 <tvaldiviana6503@student.pps.net>
2023-06-02 13:52:03 -0700

    keep doing story

---
+++ Assets/CameraFacingBillboard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+    public Camera m_Camera;
+
+    void Update()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+        Vector3 eulerAngles = transform.eulerAngles;
+        eulerAngles.z = 0;
+        transform.eulerAngles = eulerAngles;
+    }
+}
