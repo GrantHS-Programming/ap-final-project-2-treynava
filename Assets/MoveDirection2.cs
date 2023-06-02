ad21464206cd61aefe063c4478515b9072f7e5f8
Trey <tvaldiviana6503@student.pps.net>
2023-06-02 11:35:54 -0700

    still need walls

---
+++ Assets/MoveDirection2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDirection2 : MonoBehaviour
+{
+    public float speed = 1f;
+    public float smoothTime = 0.5f;
+    public Vector3 target = new Vector3(10, 4, 20);
+    Vector3 currentVelocity;
+
+    private void Update()
+    {
+        //objectToMove.transform.Translate(moveDirection * speed);
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref currentVelocity, smoothTime);
+    }
+}
---
+++ Assets/RunScriptOnTrigger2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunScriptOnTrigger2 : MonoBehaviour
+{
+    public GameObject RunScript;
+
+    void OnTriggerEnter(Collider other)
+    {
+        RunScript.GetComponent<MoveDirection2>().enabled = true;
+    }
+}
